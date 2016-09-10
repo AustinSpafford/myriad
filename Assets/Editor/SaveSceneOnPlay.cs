33f8f2e70fdab65917c48325c3ccf8ab2aba9333
Austin Spafford <austin.spafford@gmail.com>
2016-09-09 17:29:13 -0700

    Adding a SaveSceneOnPlay editor-script

---
+++ Assets/Editor/SaveSceneOnPlay.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class SaveSceneOnPlay
+{
+	static SaveSceneOnPlay ()
+	{
+		EditorApplication.playmodeStateChanged = OnPlaymodeStateChanged;
+	}
+
+	private static void OnPlaymodeStateChanged ()
+	{
+		// If the user _just_ pressed the Play button.
+		if (EditorApplication.isPlayingOrWillChangePlaymode &&
+			!EditorApplication.isPlaying)
+		{
+			EditorSceneManager.SaveOpenScenes();
+
+			EditorApplication.SaveAssets();
+		}
+	}
+}
