2d43ba487e1ddbb76a75ddb7f288070d179a674d
Austin Spafford <austin.spafford@gmail.com>
2016-10-17 18:42:19 -0700

    Stubbing the AudioLabelTracker and attaching music the player's head

---
+++ Assets/Myriad/HeirarchyUtils/ReparentOnAwake.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReparentOnAwake : MonoBehaviour
+{
+	public Transform NewParent = null;
+
+	public void Awake()
+	{
+		if (NewParent != null)
+		{
+			transform.SetParent(NewParent);
+
+			// Signify that we've completed our task and are now dormant.
+			NewParent = null;
+			enabled = false;
+		}
+	}
+}
