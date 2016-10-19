854129b3466dd60c3e155ebf27d594a9a23d0477
Austin Spafford <austin.spafford@gmail.com>
2016-10-19 08:45:49 -0700

    Broadcasting the audio-label events

--- Assets/Myriad/Music/AudioLabelBroadcaster.cs
+++ Assets/Myriad/Music/AudioLabelBroadcaster.cs
@@ -1,13 +1,50 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
+public enum AudioLabelEventType
+{
+	ImmediateLabel,
+	IntervalLabelStarting,
+	IntervalLabelEnding,
+}
+
+public class AudioLabelEventArgs : EventArgs
+{
+	public AudioLabelBroadcaster Broadcaster;
+	public bool BroadcasterIsSeeking; // True when events are being rapidly sent out while seeking the play-head forward over a long distance/time.
+	
+	public AudioSource CorrespondingAudio;
+	
+	public float EventTime;
+	public AudioLabelEventType EventType;
+	public string LabelName;
+}
+
+public class AudioLabelEventStreamRestartingEventArgs : EventArgs
+{
+	public AudioLabelBroadcaster Broadcaster;
+}
+
 [RequireComponent(typeof(AudioSource))]
 public class AudioLabelBroadcaster : MonoBehaviour
 {
 	public TextAsset LabelsFile = null;
+
+	public string[] IgnoredLabelPrefixes = new string[0];
+
+	public bool DebugEnabled = false;
+
+	// The audio-label event is the main purpose for this class.
+	// It allows annotation-files to be spooled out to other systems as our sibling audio-source is played.
+	public static event EventHandler<AudioLabelEventArgs> AudioLabelEventTriggered;
+
+	// The stream-restarting event is sent out whenever the audio-clip's playhead moves 
+	// backwards in time, forcing us to throw away everything and re-seek to its new position.
+	public static event EventHandler<AudioLabelEventStreamRestartingEventArgs> AudioLabelEventStreamRestarting;
 
 	public void Awake()
 	{
@@ -21,30 +58,57 @@ public class AudioLabelBroadcaster : MonoBehaviour
 			LoadLabelsFile(LabelsFile);
 		}
 
-		// TODO Reset playback if the target-time is behind our exposed labels.
-		// NOTE: Broadcast a rewind-warning so other components know to ditch their state.
+		// If the playhead moved backwards in time such that our last-broadcast event is no longer appropriate.
+		if ((nextLabelEventIndex > 0) &&
+			(audioSource.time < sortedLabelEvents[nextLabelEventIndex - 1].EventTime))
+		{
+			if (DebugEnabled)
+			{
+				Debug.Log("Restarting event stream!");
+			}
 
-		// TODO Seek forward to the target-time, broadcasting all encountered label-events.
+			// Notify that we're restarting the event-stream.
+			{
+				var eventArgs = new AudioLabelEventStreamRestartingEventArgs()
+				{
+					Broadcaster = this,
+				};
+
+				if (AudioLabelEventStreamRestarting != null)
+				{
+					AudioLabelEventStreamRestarting(this, eventArgs);
+				}
+			}
+
+			// Re-seek to the current time, but flagging the events as being seek-spam so
+			// consumers can choose to opt-out of any expensive operations.
+			{
+				nextLabelEventIndex = 0;
+
+				BroadcastLabelEvents(
+					audioSource.time, 
+					broadcasterIsSeeking: true);
+			}
+		}
+		
+		BroadcastLabelEvents(
+			audioSource.time, 
+			broadcasterIsSeeking: false);
 	}
 
-	private enum LabelEventType
+	private struct SortableLabelEvent
 	{
-		ImmediateLabel,
-		DurationLabelStarting,
-		DurationLabelEnding,
-	}
-
-	private struct AudioLabelEvent
-	{
-		public LabelEventType EventType;
-		public string LabelName;
 		public float EventTime;
+		public AudioLabelEventType EventType;
+		public string LabelName;
 	}
 
 	private AudioSource audioSource = null;  
 
 	private TextAsset loadedLabelsFile = null;
-	private List<AudioLabelEvent> sortedLabelEvents = new List<AudioLabelEvent>();
+	private List<SortableLabelEvent> sortedLabelEvents = new List<SortableLabelEvent>();
+
+	private int nextLabelEventIndex = 0;
 
 	private void LoadLabelsFile(
 		TextAsset newLabelsFile)
@@ -76,30 +140,41 @@ public class AudioLabelBroadcaster : MonoBehaviour
 						{
 							string labelName = tokens[2];
 
-							if (startTime == endTime)
-							{
-								sortedLabelEvents.Add(new AudioLabelEvent()
-								{
-									EventType = LabelEventType.ImmediateLabel,
-									LabelName = labelName,
-									EventTime = startTime,
-								});
-							}
-							else
-							{
-								sortedLabelEvents.Add(new AudioLabelEvent()
-								{
-									EventType = LabelEventType.DurationLabelStarting,
-									LabelName = labelName,
-									EventTime = startTime,
-								});
+							bool labelIsIgnored = 
+								IgnoredLabelPrefixes
+									.Where(ignoredPrefix => labelName.StartsWith(ignoredPrefix))
+									.Any();
 
-								sortedLabelEvents.Add(new AudioLabelEvent()
+							if (labelIsIgnored == false)
+							{
+								if (startTime == endTime)
 								{
-									EventType = LabelEventType.DurationLabelEnding,
-									LabelName = labelName,
-									EventTime = endTime,
-								});
+									sortedLabelEvents.Add(new SortableLabelEvent()
+									{
+										EventTime = startTime,
+										EventType = AudioLabelEventType.ImmediateLabel,
+										LabelName = labelName,
+									});
+								}
+								else
+								{
+									// NOTE: We break the interval-labels into two separate events so they can
+									// be sorted chronologically (otherwise overlapping intervals are ambiguous).
+
+									sortedLabelEvents.Add(new SortableLabelEvent()
+									{
+										EventTime = startTime,
+										EventType = AudioLabelEventType.IntervalLabelStarting,
+										LabelName = labelName,
+									});
+
+									sortedLabelEvents.Add(new SortableLabelEvent()
+									{
+										EventTime = endTime,
+										EventType = AudioLabelEventType.IntervalLabelEnding,
+										LabelName = labelName,
+									});
+								}
 							}
 						}
 					}
@@ -109,9 +184,51 @@ public class AudioLabelBroadcaster : MonoBehaviour
 			// Chronologically sort the events.
 			// Note that we can't use the standard in-place Sort() function, because it's unstable. 
 			// However, OrderBy() is explicitly documented as being a stable sort, so it's fair-game.
-			sortedLabelEvents = new List<AudioLabelEvent>(sortedLabelEvents.OrderBy(elem => elem.EventTime));
+			sortedLabelEvents = new List<SortableLabelEvent>(sortedLabelEvents.OrderBy(elem => elem.EventTime));
+
+			nextLabelEventIndex = 0;
 
 			loadedLabelsFile = newLabelsFile;
+		}
+	}
+	
+	private void BroadcastLabelEvents(
+		float targetTime,
+		bool broadcasterIsSeeking)
+	{
+		while ((nextLabelEventIndex < sortedLabelEvents.Count) && 
+			(sortedLabelEvents[nextLabelEventIndex].EventTime <= targetTime))
+		{
+			var sourceLabelEvent = sortedLabelEvents[nextLabelEventIndex];
+
+			var eventArgs = new AudioLabelEventArgs()
+			{
+				Broadcaster = this,
+				BroadcasterIsSeeking = broadcasterIsSeeking,
+
+				CorrespondingAudio = audioSource,
+
+				EventTime = sourceLabelEvent.EventTime,
+				EventType = sourceLabelEvent.EventType,
+				LabelName = sourceLabelEvent.LabelName,
+			};
+			
+			if (AudioLabelEventTriggered != null)
+			{
+				AudioLabelEventTriggered(this, eventArgs);
+			}
+
+			if (DebugEnabled)
+			{
+				Debug.LogFormat(
+					"{0} {1:n2} {2} {3}", 
+					nextLabelEventIndex,
+					eventArgs.EventTime,
+					eventArgs.EventType,
+					eventArgs.LabelName);
+			}
+
+			++nextLabelEventIndex;
 		}
 	}
 }
