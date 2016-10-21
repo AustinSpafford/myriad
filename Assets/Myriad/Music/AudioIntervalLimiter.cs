bb8c2028335d48fd085dfbb4aece193fb5859cf6
Austin Spafford <austin.spafford@gmail.com>
2016-10-21 10:30:14 -0700

    Adding an AudioIntervalLimiter to aid in testing

---
+++ Assets/Myriad/Music/AudioIntervalLimiter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class AudioIntervalLimiter : MonoBehaviour
+{
+	public float StartTime = 0.0f;
+	public float EndTime = 999.0f;
+	
+	public void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	public void Update()
+	{
+		if (audioSource.isPlaying)
+		{
+			if (audioSource.time < StartTime)
+			{
+				audioSource.time = StartTime;
+			}
+			else if (audioSource.time > EndTime)
+			{
+				if (audioSource.loop)
+				{
+					audioSource.time = StartTime;
+				}
+				else
+				{
+					audioSource.Stop();
+				}
+			}			
+		}
+	}
+	
+	private AudioSource audioSource = null;  
+}
