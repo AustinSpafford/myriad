568ca459a774c2ac4a060a0fa18e03303025b91c
Austin Spafford <austin.spafford@gmail.com>
2016-11-16 13:18:51 -0800

    Adding an initial stab at porting to Android

---
+++ Assets/Myriad/HeirarchyUtils/ActiveOnPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class ActiveOnPlatform : MonoBehaviour
+{
+	public RuntimePlatform[] WhitelistedPlatforms = null;
+	public RuntimePlatform[] BlacklistedPlatforms = null;
+
+	public void Awake()
+	{
+		bool passesWhitelist = (
+			(WhitelistedPlatforms.Any() == false) ||
+			WhitelistedPlatforms.Contains(Application.platform));
+		
+		bool passesBlacklist = 
+			(BlacklistedPlatforms.Contains(Application.platform) == false);
+
+		bool shouldBeActive = (passesWhitelist && passesBlacklist);
+
+		// NOTE: We intentionally avoid activating inactive objects, since there 
+		// might be multiple activation-filters present.
+		if (shouldBeActive == false)
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Myriad/HeirarchyUtils/MimicDeviceRotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MimicDeviceRotation : MonoBehaviour
+{
+	public bool TrackingEnabled = true;
+
+	public void Start()
+	{
+		Input.compass.enabled = true;
+		Input.gyro.enabled = true;
+	}
+
+	public void FixedUpdate()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			TrackingEnabled = !TrackingEnabled;
+		}
+
+		if (TrackingEnabled)
+		{
+			// The device's coordinate system is right-handed, such that we must flip the z-axis.
+			// Note that he w-coordinate has to also be flipped to retain the direction of rotation.
+			// Unfortunately I'm failing to find an appropriate math-resource to clearly describe *why* this works.
+			Quaternion unitySpaceDeviceRotation = new Quaternion(
+				Input.gyro.attitude.x,
+				Input.gyro.attitude.y,
+				(-1 * Input.gyro.attitude.z),
+				(-1 * Input.gyro.attitude.w));
+		
+			transform.localRotation = (
+				Quaternion.AngleAxis(90.0f, Vector3.right) * // Rotate from looking at the ground to looking forward.
+				unitySpaceDeviceRotation);
+		}
+	}
+}
